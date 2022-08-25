78bbb71e8f1aa88a169f76f28c939d23f3d8a8a4
routabhi1995 <routabhi1995@gmail.com>
2022-08-25 12:40:09 +0530

    Day8

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ReadWriteIni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            IniFile settingsIni = new IniFile (@".\\TestData.Ini");
+ 
+            string executionMode = settingsIni.Read ("BrowserName", "BrowserInfo").ToLower ();
+            Console.WriteLine (executionMode);
+ 
+            string WebdriverToUse = settingsIni.Read ("WebDriverLocation", "BrowserInfo").ToLower ();
+            Console.WriteLine (WebdriverToUse);
+            settingsIni.Write("TimeOut","4000","BrowserInfo");
+           
+
+            string WebdriverTotime = settingsIni.Read ("BrowserName", "BrowserInfo");
+            Console.WriteLine (WebdriverTotime);
+            settingsIni.Read("TimeOut","BrowserInfo");
+            settingsIni.Write("BrowserName","IE","BrowserInfo");
+            settingsIni.Write("Url","www.facebook.com","AppUnderTest");
+            //settingsIni.Write("Url","www.facebook.com","AppUnderTest");
+        }
+    }
+}
